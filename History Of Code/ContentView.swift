e6f8dd384ae947daf20d8103e52884d004076e37
miguelcastiyo <44275086+miguelcastiyo@users.noreply.github.com>
2023-02-21 19:17:54 -0800

    Initial Commit

---
+++ History Of Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  History Of Code
+//
+//  Created by miguel on 2/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ History Of Code/History_Of_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  History_Of_CodeApp.swift
+//  History Of Code
+//
+//  Created by miguel on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct History_Of_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
