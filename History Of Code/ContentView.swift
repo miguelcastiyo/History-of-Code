da1cb310d7e19c5873b6040d92f56c5ec636b2ef
miguelcastiyo <44275086+miguelcastiyo@users.noreply.github.com>
2023-02-25 20:49:46 -0800

    Initial commit.

--- History Of Code/ContentView.swift
+++ History Of Code/ContentView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack{
+                Text("1 / 10")
+                    .font(.callout)
+                    .multilineTextAlignment(.leading)
+                    .padding()
+                Text("What was the first computer bug?")
+                    .font(.largeTitle)
+                    .bold()
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                    }, label: {
+                        Text("Button")
+                    })
+                }
+            }
+        }
+        .foregroundColor(.white)
     }
+    
+    
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
