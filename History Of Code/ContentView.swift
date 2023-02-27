b14f613a9fda44e75b59256daf9feb42a49c1607
Miguel Castillo <castillo_m17@icloud.com>
2023-02-26 18:12:39 -0800

    Added some buttons.

--- History Of Code/ContentView.swift
+++ History Of Code/ContentView.swift
@@ -28,7 +28,45 @@ struct ContentView: View {
                     Button(action: {
                         print("Tapped on Choice 1")
                     }, label: {
-                        Text("Button")
+                        Text("Ant")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width:4)
+                    })
+                    // Start of Button 2
+                    Button(action: {
+                        print("Tapped on Choice 2")
+                    }, label: {
+                        Text("Beetle")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width:4)
+                    })
+                    // Start of Button 3
+                    Button(action: {
+                        print("Tapped on Choice 3")
+                    }, label: {
+                        Text("Moth")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width:4)
+                    })
+                    // Start of Button 4
+                    Button(action: {
+                        print("Tapped on Choice 4")
+                    }, label: {
+                        Text("Fly")
+                            .font(.body)
+                            .bold()
+                            .multilineTextAlignment(.center)
+                            .padding()
+                            .border(accentColor, width:4)
                     })
                 }
             }
