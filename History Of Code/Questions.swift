be25d8c5689ec33258237f5bc29e423cb412a99d
Miguel Castillo <castillo_m17@icloud.com>
2023-03-21 20:19:45 -0700

    Added Questions.swift

---
+++ History Of Code/Questions.swift
@@ -0,0 +1,19 @@
+//
+//  Questions.swift
+//  History Of Code
+//
+//  Created by Miguel Castillo on 3/21/23.
+//
+
+import Foundation
+
+struct Question {
+    
+    // Properties
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+    
+    
+    
+}
--- History Of Code/ContentView.swift
+++ History Of Code/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let question = Question(questionText: "What was the first computer bug?", possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"], correctAnswerIndex: 2)
+    
     let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
     let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     
@@ -19,7 +22,7 @@ struct ContentView: View {
                     .font(.callout)
                     .multilineTextAlignment(.leading)
                     .padding()
-                Text("What was the first computer bug?")
+                Text(question.questionText)
                     .font(.largeTitle)
                     .bold()
                     .multilineTextAlignment(.leading)
@@ -28,7 +31,7 @@ struct ContentView: View {
                     Button(action: {
                         print("Tapped on Choice 1")
                     }, label: {
-                        Text("Ant")
+                        Text(question.possibleAnswers[0])
                             .font(.body)
                             .bold()
                             .multilineTextAlignment(.center)
@@ -37,7 +40,7 @@ struct ContentView: View {
                     })
                     // Start of Button 2
                     Button(action: {
-                        print("Tapped on Choice 2")
+                        print(question.possibleAnswers[1])
                     }, label: {
                         Text("Beetle")
                             .font(.body)
@@ -50,7 +53,7 @@ struct ContentView: View {
                     Button(action: {
                         print("Tapped on Choice 3")
                     }, label: {
-                        Text("Moth")
+                        Text(question.possibleAnswers[2])
                             .font(.body)
                             .bold()
                             .multilineTextAlignment(.center)
@@ -61,7 +64,7 @@ struct ContentView: View {
                     Button(action: {
                         print("Tapped on Choice 4")
                     }, label: {
-                        Text("Fly")
+                        Text(question.possibleAnswers[3])
                             .font(.body)
                             .bold()
                             .multilineTextAlignment(.center)
